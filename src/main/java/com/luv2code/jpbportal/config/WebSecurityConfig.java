63b8f725e8b990fadbbbdbebeb59a888b0828fb5
Serhii <donetsk55555@gmail.com>
2024-09-15 01:30:33 +0300

    19 Custom Auth Success Handler

--- src/main/java/com/luv2code/jpbportal/config/WebSecurityConfig.java
+++ src/main/java/com/luv2code/jpbportal/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,10 +16,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailService customUserDetailService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Autowired
-    public WebSecurityConfig(CustomUserDetailService customUserDetailService) {
+    public WebSecurityConfig(CustomUserDetailService customUserDetailService,
+                             CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
         this.customUserDetailService = customUserDetailService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
     }
 
     private final String[] publicUrl = {"/",
@@ -47,6 +51,15 @@ public class WebSecurityConfig {
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+
+        http.formLogin(form -> form.loginPage("/login").permitAll()
+                        .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
--- src/main/java/com/luv2code/jpbportal/services/UsersService.java
+++ src/main/java/com/luv2code/jpbportal/services/UsersService.java
@@ -7,6 +7,7 @@ import com.luv2code.jpbportal.repository.JobSeekerProfileRepository;
 import com.luv2code.jpbportal.repository.RecruiterProfileRepository;
 import com.luv2code.jpbportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,19 +18,23 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UsersService(UsersRepository usersRepository,
                         JobSeekerProfileRepository jobSeekerProfileRepository,
-                        RecruiterProfileRepository recruiterProfileRepository) {
+                        RecruiterProfileRepository recruiterProfileRepository,
+                        PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         int userTypeId = users.getUserTypeId().getUserTypeId();
         if (userTypeId == 1) {
