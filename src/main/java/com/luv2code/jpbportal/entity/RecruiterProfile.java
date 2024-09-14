7d1a14985dcd0bd432cf864844096c54f844fad3
Serhii <donetsk55555@gmail.com>
2024-09-14 15:12:28 +0300

    15 Create entity recruiter and seeker

---
+++ src/main/java/com/luv2code/jpbportal/entity/RecruiterProfile.java
@@ -0,0 +1,139 @@
+package com.luv2code.jpbportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recruiter_profile")
+public class RecruiterProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "company")
+    private String company;
+
+    @Column(name = "profile_photo", nullable = true, length = 64)
+    private String profilePhoto;
+
+    public RecruiterProfile() {
+    }
+
+    public RecruiterProfile(int userAccountId, Users userId, String firstName, String lastName, String city, String state, String country, String company, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.company = company;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "RecruiterProfile{" +
+               "userAccountId=" + userAccountId +
+               ", userId=" + userId +
+               ", firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", city='" + city + '\'' +
+               ", state='" + state + '\'' +
+               ", country='" + country + '\'' +
+               ", company='" + company + '\'' +
+               ", profilePhoto='" + profilePhoto + '\'' +
+               '}';
+    }
+}
---
+++ src/main/java/com/luv2code/jpbportal/entity/Skills.java
@@ -0,0 +1,10 @@
+package com.luv2code.jpbportal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skills")
+public class Skills {
+
+}
