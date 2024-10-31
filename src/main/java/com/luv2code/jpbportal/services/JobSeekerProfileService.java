92e686fa5d6cb43e5c7b8d6c1770d172199569d6
Serhii <donetsk55555@gmail.com>
2024-10-31 19:34:14 +0200

    45 Controller for searching

--- src/main/java/com/luv2code/jpbportal/services/JobSeekerProfileService.java
+++ src/main/java/com/luv2code/jpbportal/services/JobSeekerProfileService.java
@@ -1,8 +1,14 @@
 package com.luv2code.jpbportal.services;
 
 import com.luv2code.jpbportal.entity.JobSeekerProfile;
+import com.luv2code.jpbportal.entity.Users;
 import com.luv2code.jpbportal.repository.JobSeekerProfileRepository;
+import com.luv2code.jpbportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +17,12 @@ import java.util.Optional;
 public class JobSeekerProfileService {
 
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final UsersRepository usersRepository;
 
     @Autowired
-    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository, UsersRepository usersRepository) {
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Optional<JobSeekerProfile> getOne(Integer id) {
@@ -23,5 +31,17 @@ public class JobSeekerProfileService {
 
     public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
         return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+
+    public JobSeekerProfile getCurrentSeekerProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AbstractAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<JobSeekerProfile> seekerProfile = getOne(users.getUserId());
+            return seekerProfile.orElse(null);
+        } else {
+            return null;
+        }
     }
 }
